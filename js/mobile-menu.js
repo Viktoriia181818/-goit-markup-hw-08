02981be1ba2f3ab72e9ea77805b1ea3d527bd8c7
Viktoriia181818 <vcibulskaa4@gmail.com>
2022-11-21 11:00:12 +0200

    сщьше

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -13,3 +13,9 @@
   });
 })();
 
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
